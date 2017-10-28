8983983058f82e30d57676379c8f0244d56f9898
tawashley <trisashley@gmail.com>
2017-10-28 18:28:44 +0100

    feat: scaled, centered world map

--- src/WorldMap.js
+++ src/WorldMap.js
@@ -2,11 +2,17 @@ import React, {
     Component
 } from 'react';
 import './App.css';
+import worldMap from './worldmap.jpg';
 
 class SplashScreen extends Component {
     render() {
         return (
-            <section class="game-section game-section--world-map"></section>
+            <section class="game-section">
+                <section className='level-overlay'>
+                    
+                </section>
+                <img src={worldMap} className='world-map' />
+            </section>
         )
     }
 }
