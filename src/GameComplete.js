20466ee4e72e0955a01703cbced2c2807fd73f14
tawashley <trisashley@gmail.com>
2017-10-29 04:34:16 +0000

    feat: Show data on game over screen

--- src/GameComplete.js
+++ src/GameComplete.js
@@ -7,15 +7,59 @@ import questionComplete from './questcomplete.svg';
 import scrollGameOver from './scroll-gameover.png';
 import vikingVictory from './vikingvictory.gif';
 
+import apiService from './apiService';
+
 class GameComplete extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            gameData: {
+                Levels: [
+                    {
+                        Inputs: [{
+                            Value: '______'
+                        }]
+                    }
+                ]
+            }
+        }
+
+        apiService
+            //.getGameData(this.props.match.params.gameId)
+            .getGameData('899aa052-63a2-4880-9d11-a4c280582597')
+            .then(this.setInitialState.bind(this))
+    }
+
+    setInitialState(response) {
+        console.log(response);
+
+        this.setState({
+            gameData: response
+        })
+    }
+            
     render() {
+        var text = this.state.gameData.Levels.map((levelData) => {
+            return (
+                <p>
+                    {levelData.Inputs[0].Name} : {levelData.Inputs[0].Value}
+                </p>
+            )
+        });
+
+        console.log(text);
+
         return (
-            <section style={{ textAlign: 'center', marginTop: '30px' }}>
+            <section style={{ textAlign: 'center', marginTop: '30px' , display: (this.state.gameData.length === 0) ? 'none' : 'block' }}>
                 <div className='game-complete-text'>
-                    <strong style={{ fontSize: '24px'}}>Congratulations NAME HERE</strong>
+                    <strong style={{ fontSize: '24px'}}>Congratulations {this.state.gameData.Levels[0].Inputs[0].Value}</strong>
                     <p>
-                        Your quest is over, and we have received your complaint 
+                        Your quest is over, and we have received your complaint of thy words, 
                     </p>
+
+                    { text }
+
                     <p>
                         Now continue on your way
                     </p>
