75e98161b88c2ebe6b68537b01789f91af01921d
Laurent Humbert <Laurent.Humbert@codecomputerlove.com>
2017-10-29 11:06:53 +0000

    Added clouds to contact leve

--- src/PhoneNumberLevel.js
+++ src/PhoneNumberLevel.js
@@ -112,8 +112,28 @@ class PhoneNumberLevel extends Component {
             <section style={{ textAlign: 'center', marginTop: '150px' }}>
                 <img src={levelOneLogo} alt='' style={{ maxWidth: '420px', marginBottom: '60px'}} />                
                 {button}    
-                
-                <div className="phone-number-container">
+                <div className='clouds-container'>
+                    <div className="x1">
+                        <div className="cloud"></div>
+                    </div>
+
+                    <div className="x2">
+                        <div className="cloud"></div>
+                    </div>
+
+                    <div className="x3">
+                        <div className="cloud"></div>
+                    </div>
+
+                    <div className="x4">
+                        <div className="cloud"></div>
+                    </div>
+
+                    <div className="x5">
+                        <div className="cloud"></div>
+                    </div>
+                </div>
+                <div className="phone-number-container ">
                     <textarea className="speechbubble-phone" value={this.state.phoneNumber} readOnly />
                     <button className='lvl-phone-button' onClick={this.startRecording.bind(this)}>RECORD</button>
                     
